a70933d533f51a353928fb829ee901f92492703d
Den Degtiarev <aior@mail.ru>
2021-02-17 20:00:16 +0300

    4th task

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -1,6 +1,22 @@
 export const CounterButton = {
   // Шаблон потребуется отредактировать
-  template: '<button type="button"></button>',
+  template: '<button type="button" @click="increment(count)" :count="count">{{ count }}</button>',
+  props: {
+    count: {
+      type: Number,
+      default: 0
+    }
+  },
+  model: {
+    prop: 'count',
+    event: 'increment'
+  },
+  methods: {
+    increment(newValue) {
+      newValue++
+      this.$emit('increment', newValue)
+    }
+  }
 
   // Компонент должен иметь входной параметр
 
